5d5e3b2e707df9fa5c54eaefbbe06a5dac07b69c
Felype Henrique <felypehenrique25@gmail.com>
2021-10-29 17:53:20 -0300

    add Bootstrap

--- index.js
+++ index.js
@@ -4,27 +4,8 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-app.get("/:nome/:lang", (req, res) => {
-  var nome = req.params.nome;
-  var lang = req.params.lang;
-  var exibirMsg = false;
-
-  var produtos = [
-    {nome: "Doritos", preco: 3.14},
-    {nome: "Coca-cola", preco: 5},
-    {nome:"Leite", preco: 1.45},
-    {nome: "Carne", preco: 15},
-    {nome: "Redbull", preco: 6},
-    {nome:"Nescal", preco: 4}
-  ]
-  res.render("index", {
-    nome: nome,
-    lang: lang,
-    empresa: "guia do programador",
-    inscritos: 8000,
-    msg: exibirMsg,
-    produtos: produtos
-  });
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
 app.listen(8080, () => {
