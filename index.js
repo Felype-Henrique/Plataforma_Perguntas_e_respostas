46e96f13e75fdb8412f889522c5a2cd1195cd9a3
Felype Henrique <felypehenrique25@gmail.com>
2021-10-29 10:03:33 -0300

    repeticao com EJS

--- index.js
+++ index.js
@@ -7,12 +7,22 @@ app.get("/:nome/:lang", (req, res) => {
   var nome = req.params.nome;
   var lang = req.params.lang;
   var exibirMsg = false;
+
+  var produtos = [
+    {nome: "Doritos", preco: 3.14},
+    {nome: "Coca-cola", preco: 5},
+    {nome:"Leite", preco: 1.45},
+    {nome: "Carne", preco: 15},
+    {nome: "Redbull", preco: 6},
+    {nome:"Nescal", preco: 4}
+  ]
   res.render("index", {
     nome: nome,
     lang: lang,
     empresa: "guia do programador",
     inscritos: 8000,
-    msg: exibirMsg
+    msg: exibirMsg,
+    produtos: produtos
   });
 });
 
